f00cf73b2c56851446736acb1212fd0410bcbd8e
Tanfitra <tanfitraadhim@gmail.com>
2021-09-19 14:38:39 +0700

    fix cmd nrd

--- commands/nrd.js
+++ commands/nrd.js
@@ -10,14 +10,13 @@ module.exports = {
         message.channel.bulkDelete(1)
 
         const freeembed = new MessageEmbed()
-            .setTitle('☁  Nord VPN  ☁')
+            .setTitle('☁ Nord VPN ☁')
             .addField('Perhatian', botInfo['perhatian-nord'])
             .addField('Pricelist :', botInfo['pricelist-nord'])
             .addField('Keterangan :', botInfo.keterangan)
-            .setFooter('Nord Pricelist')
+            .setFooter('Nord VPN Pricelist')
             .setColor('FFB319')
             .setThumbnail('https://cdn.discordapp.com/attachments/882302455642226754/887167320072523846/882622321326714880.png')
-            .
         message.channel.send(freeembed)
         
     }
